b744388ccd6aabba472cf0e5ca9c0869680738d6
kbitykot <caitlynh4704@gmail.com>
2026-05-02 01:08:08 -0400

    Add project files.

---
+++ Kitty Generator 3.0/Kitty.cpp
@@ -0,0 +1,1 @@
+#include "Kitty.h"
---
+++ Kitty Generator 3.0/Kitty.h
@@ -0,0 +1,37 @@
+/*Name: Caitlyn Swick
+CIS 1202-501
+Development began 5/2/26
+Uploaded to GitHub for fun. Check it out.*/
+
+#ifndef KITTY_H
+#define KITTY_H
+
+#include<string>
+#include<vector>
+
+enum class TailLength { Short, Medium, Long };
+enum class FurPattern { Plain, Point, Speckles, Spots, Patches, Stripes};
+enum class FurLength { Hairless, Short, Average, Long, ExtraFluffy};
+enum class KittenSize { Tiny, Small, Average, Big };
+enum class AdultSize { Small, Average, Big, Monster };
+
+class Kitty {
+private:
+	std::string name;
+	std::vector<std::string> furColors;
+	TailLength tail;
+	std::vector<std::string> personality;
+	int age;
+	bool isKitten, hasHeterochromia;
+	std::vector<std::string> eyeColors;
+	std::vector<FurPattern> patterns;
+	FurLength furLength;
+	KittenSize kSize;
+	AdultSize aSize;
+	std::string accessory, gender;
+
+public:
+	
+};
+
+#endif
